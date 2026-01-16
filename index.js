87b68bb2cbe6b67b3768fb2cd05ad8ed668c9ab7
oscar1771 <ozarate8181@gmail.com>
2026-01-16 09:58:29 +0100

    Potion Creator first version uploaded

---
+++ index.js
@@ -0,0 +1,14 @@
+const { testIngredients } = require ('./testIngredients');
+const { testIngredients2 } = require ('./testIngredients2');
+
+
+const { Cauldron }  = require ('./cauldron');
+console.log(testIngredients2);
+
+
+//const potion  = Cauldron.createPotion(testIngredients);
+const potion2 = Cauldron.createPotion(testIngredients2);
+
+
+//console.log(potion);
+console.log(potion2);
