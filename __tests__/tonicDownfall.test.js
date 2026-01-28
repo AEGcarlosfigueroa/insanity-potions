fb94518a978938b4341e4c9f9ce39118ea275fa3
Carlos Aguilar <carlos.figueroa@ikasle.aeg.eus>
2026-01-28 08:53:47 +0100

    [ADD] tonic downfall tests

---
+++ __tests__/tonicDownfall.test.js
@@ -0,0 +1,219 @@
+const { Cauldron }  = require ('./../cauldron.js');
+
+describe("tests for generation of tonic downfall", () => {
+    test("When there is only one ingredient in input, the resulting potion should be tonic downfall", () => {
+        const ingredients = [
+            {
+              "_id": "6702b39d76863c206a48cccc",
+              "name": "Ironbark Berry",
+              "description": "A hard berry that enhances hit points by a small amount.",
+              "value": 25,
+              "effects": [
+                "increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_3.webp",
+              "type": "ingredient"
+            },
+        ];
+
+        const expectedName = "Tonic of Downfall";
+
+        const potion = Cauldron.createPotion(ingredients);
+
+        expect(potion.name).toBe(expectedName);
+    })
+    test("When there are two compatible ingredients in input, the resulting potion must not be tonic downfall", () => {
+        const ingredients = [
+            {
+              "_id": "6702b39d76863c206a48cccc",
+              "name": "Ironbark Berry",
+              "description": "A hard berry that enhances hit points by a small amount.",
+              "value": 25,
+              "effects": [
+                "increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_3.webp",
+              "type": "ingredient"
+            },
+            {
+              "_id": "6702b39d76863c206a48cccd",
+              "name": "Moonleaf",
+              "description": "A mystical leaf that offers a slight increase in hit points.",
+              "value": 8,
+              "effects": [
+                "lesser_increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_4.webp",
+              "type": "ingredient"
+            }
+        ];
+
+        const notExpectedName = "Tonic of Downfall";
+
+        const potion = Cauldron.createPotion(ingredients);
+
+        expect(potion.name).not.toBe(notExpectedName);
+    })
+    test("When there are three compatible ingredients in input, the resulting potion must not be tonic downfall", () => {
+        const ingredients = [
+            {
+              "_id": "6702b39d76863c206a48cccc",
+              "name": "Ironbark Berry",
+              "description": "A hard berry that enhances hit points by a small amount.",
+              "value": 25,
+              "effects": [
+                "increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_3.webp",
+              "type": "ingredient"
+            },
+            {
+              "_id": "6702b39d76863c206a48cccd",
+              "name": "Moonleaf",
+              "description": "A mystical leaf that offers a slight increase in hit points.",
+              "value": 8,
+              "effects": [
+                "lesser_increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_4.webp",
+              "type": "ingredient"
+            },
+            {
+                "_id":"6702b39d76863c206a48cccc",
+                "name":"Ironbark Berry",
+                "description":"A hard berry that enhances hit points by a small amount.",
+                "value":25,
+                "effects":[
+                    "lesser_increase_hit_points"
+                ],
+                "image":"/images/ingredients/increase/increase_3.webp",
+                "type":"ingredient"
+            }
+        ];
+
+        const notExpectedName = "Tonic of Downfall";
+
+        const potion = Cauldron.createPotion(ingredients);
+
+        expect(potion.name).not.toBe(notExpectedName);
+    })
+    test("When there are four compatible ingredients in input, the resulting potion must not be tonic downfall", () => {
+        const ingredients = [
+            {
+              "_id": "6702b39d76863c206a48cccc",
+              "name": "Ironbark Berry",
+              "description": "A hard berry that enhances hit points by a small amount.",
+              "value": 25,
+              "effects": [
+                "increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_3.webp",
+              "type": "ingredient"
+            },
+            {
+              "_id": "6702b39d76863c206a48cccd",
+              "name": "Moonleaf",
+              "description": "A mystical leaf that offers a slight increase in hit points.",
+              "value": 8,
+              "effects": [
+                "lesser_increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_4.webp",
+              "type": "ingredient"
+            },
+            {
+                "_id":"6702b39d76863c206a48cccc",
+                "name":"Ironbark Berry",
+                "description":"A hard berry that enhances hit points by a small amount.",
+                "value":25,
+                "effects":[
+                    "lesser_increase_hit_points"
+                ],
+                "image":"/images/ingredients/increase/increase_3.webp",
+                "type":"ingredient"
+            },
+            {
+                "_id":"6702b39d76863c206a48ccca",
+                "name":"Heartroot",
+                "description":"A rare root known to strengthen the body's vitality permanently.",
+                "value":275,
+                "effects":[
+                    "greater_increase_hit_points"
+                ],
+                "image":"/images/ingredients/increase/increase_1.webp",
+                "type":"ingredient"
+            }
+        ];
+
+        const notExpectedName = "Tonic of Downfall";
+
+        const potion = Cauldron.createPotion(ingredients);
+
+        expect(potion.name).not.toBe(notExpectedName);
+    })
+    test("When there are five ingredients in input, the resulting potion should be tonic downfall", () => {
+        const ingredients = [
+            {
+              "_id": "6702b39d76863c206a48cccc",
+              "name": "Ironbark Berry",
+              "description": "A hard berry that enhances hit points by a small amount.",
+              "value": 25,
+              "effects": [
+                "increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_3.webp",
+              "type": "ingredient"
+            },
+            {
+              "_id": "6702b39d76863c206a48cccd",
+              "name": "Moonleaf",
+              "description": "A mystical leaf that offers a slight increase in hit points.",
+              "value": 8,
+              "effects": [
+                "lesser_increase_hit_points"
+              ],
+              "image": "/images/ingredients/increase/increase_4.webp",
+              "type": "ingredient"
+            },
+            {
+                "_id":"6702b39d76863c206a48cccc",
+                "name":"Ironbark Berry",
+                "description":"A hard berry that enhances hit points by a small amount.",
+                "value":25,
+                "effects":[
+                    "lesser_increase_hit_points"
+                ],
+                "image":"/images/ingredients/increase/increase_3.webp",
+                "type":"ingredient"
+            },
+            {
+                "_id":"6702b39d76863c206a48ccca",
+                "name":"Heartroot",
+                "description":"A rare root known to strengthen the body's vitality permanently.",
+                "value":275,
+                "effects":[
+                    "greater_increase_hit_points"
+                ],
+                "image":"/images/ingredients/increase/increase_1.webp",
+                "type":"ingredient"
+            },
+            {
+                "_id":"6702b39d76863c206a48ccca",
+                "name":"Heartroot",
+                "description":"A rare root known to strengthen the body's vitality permanently.",
+                "value":275,
+                "effects":[
+                    "greater_increase_hit_points"
+                ],
+                "image":"/images/ingredients/increase/increase_1.webp",
+                "type":"ingredient"
+            }
+        ];
+
+        const expectedName = "Tonic of Downfall";
+
+        const potion = Cauldron.createPotion(ingredients);
+
+        expect(potion.name).toBe(expectedName);
+    })
+})
